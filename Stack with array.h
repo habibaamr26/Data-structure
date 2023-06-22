aed246487f47551a71fbeab523171dde916790ab
habibaamr26 <115307211+habibaamr26@users.noreply.github.com>
2023-06-23 00:23:11 +0300

    Update Stack with array.h

--- Stack with array.h
+++ Stack with array.h
@@ -1,4 +1,5 @@
 
+
 #include <iostream>
 using namespace std;
 const int Size = 5;
@@ -8,39 +9,44 @@ class Stack
 	int top;
 	t array[Size];
 public:
-	Stack()
-	{
-		top = -1;
-	 }
+	Stack() :top(-1) {}
+
 	void Push(t item)                  //insert item
 	{
-		if (top == Size-1)                                          //to check is full?   
-			cout << "error ! The stack is full" << endl;
+		if (top == Size - 1)                                          //to check is full?   
+			cout << "Error ! The stack is full" << endl;
 		else
 		{
 			top++;
 			array[top] = item;
 		}
 	}
-	//function to only remove element
-	void Remove()
+
+	bool Isempty()
 	{
-		if (top == -1)                                                  // to check is empty?
-			cout << "error ! The stack is empty" << endl;
+		
+		return top<0;
+	}
+
+	//function to only remove element
+	void pop()
+	{
+		if (Isempty())                                                  // to check is empty?
+			cout << "Error ! The stack is empty" << endl;
 		else
 		{
 			top--;
 		}
-	}
+	} 
 	//function to remove and return top
 	// overload
-	void pop(t &element)
+	void pop(t& element)
 	{
-		if (top == -1)                                                   
+		if (Isempty())
 			cout << "error ! The stack is empty" << endl;
 		else
 		{
-			
+
 			element = array[top];
 			top--;
 		}
@@ -48,19 +54,19 @@ public:
 
 	void Gettop()
 	{
-		if (top == -1)                                              
+		if (Isempty())
 			cout << "error ! The stack is empty" << endl;
 		else
 		{
-             cout << array[top] << endl;
+			cout << array[top] << endl;
 		}
 	}
 
 	void Print()
 	{
-		for (int i = top; i >=0 ; i--)                    //increment loop don’t show stack in wright way
+		for (int i = top; i >= 0; i--)                    //increment loop donâ€™t show stack in right way
 		{
-			cout << array[i] << " "  ;
+			cout << array[i] << " ";
 		}
 		cout << endl;
 	}
