ad5d4046f469252fb50619912b02ccf071f5471f
habibaamr26 <115307211+habibaamr26@users.noreply.github.com>
2023-06-24 19:27:04 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,142 +1,69 @@
+
+//Implement queue using array
 #include <iostream>
 using namespace std;
-
-  //Implement queue by liked list
-
-struct Node
-{
-	int data;
-	Node* next;
-};
-
+const int Size = 5;
+template <class t>
 class Queue
 {
-	Node* front;             //delete
-	Node* rear;             //insert
+	t array[Size];
+	int rear, front;                 
 public:
-	Queue()                          
-	{
-		front = rear =NULL;
-	}
+      Queue():rear(-1),front(-1){}
 
-	Node* Creat()
-	{
-		Node* pointer = new Node;
-		return pointer;
-	}
+	  bool isEmpty()
+	  {
+		  return rear == -1;
+	  }
 
-	bool Is_empty()
-	{ 
-		if (front ==NULL)
-			return 1;
-		else return 0;
-	}
+	  bool isFull()
+	  {
+		  return rear == Size-1;
+	  }
 
-	bool Is_full()
-	{
-		Node* pointer = Creat();
-		if (pointer == NULL)
-			return 1;
-		else return 0;
-	}
+	  void enqueu(t item)                         //to add a new element
+	  {
+		  if (isEmpty())
+		      front++; 
+		  
+		  else if (isFull())
+		  {
+			  cout << "Queue is full" << endl;
+			   return ;
+		  }
+	     rear++;
+	     array[rear] = item;
+	  }
 
-	
-	void Enqueue(int item)                 //to insert new element
-	{
-		Node* newelement = Creat();
-		newelement->data = item;
-		newelement->next = NULL;
-		if (Is_empty())
-		{
-			front = rear = newelement;
-			
-		}
-		else
-		{
-			rear->next = newelement;
-			rear = newelement;
-		}
+	  void dequeue()                                    // to delete an element
+	  {
+		  if (isEmpty())
+		  {
+			  cout << "Queue is empty" << endl;
+			  return;
+		  }
+		  else
+		  {
+			  front++;
+		  }
+	  }
 
-	}
+	  t getFront()
+	  {
+		  return array[front];
+	  }
 
-	int Dequeue()                         //delet elemenet from front
-	{
-		int temp;
-		Node* remove = Creat();
-		remove = front;
-		temp = remove->data;
-		if (Is_empty())
-			cout << "there is no element to remove" << endl;
-		else if (front == rear)
-		{
-			front = rear = NULL;
-		}
-		else
-		{
-			front = front->next;
-		}
-		delete(remove);
-		return temp;
-	}
+	  t getRear()
+	  {
+		  return array[rear];
+	  }
 
-	int GetFront()                // to get front element
-	{
-		return front->data;
-	}
-
-	int GetRear()                // to get rear element
-	{
-		return rear->data;
-	}
-
-	void Display()
-	{
-		if (front == NULL)
-			cout << "there is no item to display" << endl;
-		else
-		{
-			Node* access = Creat();
-			access = front;
-			while (access != NULL)
-			{
-				cout << access->data << " ";
-				access = access->next;
-			}
-			cout << endl;
-		}
-	}
-
-	int Count()                            //to count number of element 
-	{
-		int counter = 0;
-		Node* access = Creat();
-		access = front;
-		while (access != NULL)
-		{
-			counter++;
-			access = access->next;
-		}
-		return counter;
-	}
-
-	bool Is_found(int item)
-	{
-		bool p = 0;
-		Node* access = Creat();
-		access = front;
-		while (access != NULL)
-		{
-			if (access->data == item)
-				p = 1;
-			access = access->next;
-		}
-		return p;
-	}
-
-	void Clear()                // to delete all elemennt from the queue
-	{
-		while (!Is_empty())
-			Dequeue();
-	}
+	  void display()
+	  {
+		  for (int i = front; i <= rear; i++)
+		  {
+			  cout << array[i] << " ";
+		  }
+		  cout << endl;
+	  }
 };
-
